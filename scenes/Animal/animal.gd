8b63792d97ebea1f333030505afa8c7eb057e6bf
Evan Roberts <eroberts1998@icloud.com>
2025-02-26 20:08:22 -0500

    add impulse logic to launch animal on release

--- scenes/Animal/animal.gd
+++ scenes/Animal/animal.gd
@@ -1,20 +1,29 @@
 extends RigidBody2D
 
 @onready var label: Label = $Label
+@onready var arrow: Sprite2D = $Arrow
+@onready var stretch_sound: AudioStreamPlayer2D = $StretchSound
+@onready var launch_sound: AudioStreamPlayer2D = $LaunchSound
 
 enum ANIMAL_STATE {READY, DRAG, RELEASE}
 
 const DRAG_LIM_MAX : Vector2 = Vector2(0, 60)
 const DRAG_LIM_MIN : Vector2 = Vector2(-60, 0)
+const IMPULSE_MULT: float = 20.0
+const IMPULSE_MAX : float = 1200.0
 
 var _start : Vector2 = Vector2.ZERO
 var _drag_start : Vector2 = Vector2.ZERO
 var _dragged_vector : Vector2 = Vector2.ZERO
+var _last_dragged_vector : Vector2 = Vector2.ZERO
+var _arrow_scale_x: float = 0.0
 
 var _state : ANIMAL_STATE = ANIMAL_STATE.READY
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	_arrow_scale_x = arrow.scale.x
+	arrow.hide()
 	_start = position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -23,12 +32,25 @@ func _physics_process(delta: float) -> void:
 	label.text = "%s\n" % ANIMAL_STATE.keys()[_state]
 	label.text += "%.2f,%.1f" % [_dragged_vector.x, _dragged_vector.y]
 
+func get_impulse() -> Vector2:
+	return _dragged_vector * -1 * IMPULSE_MULT
+
+func set_release() -> void:
+	arrow.hide()
+	freeze = false
+	apply_central_impulse(get_impulse())
+	launch_sound.play()
+	
+func set_drag() -> void:
+	_drag_start = get_global_mouse_position()
+	arrow.show()
+
 func set_new_state(new_state: ANIMAL_STATE) -> void:
 	_state = new_state
 	if _state == ANIMAL_STATE.RELEASE:
-		freeze = false
+		set_release()
 	elif _state == ANIMAL_STATE.DRAG:
-		_drag_start = get_global_mouse_position()
+		set_drag()
 
 func detect_release() -> bool:
 	if _state == ANIMAL_STATE.DRAG:
@@ -37,10 +59,24 @@ func detect_release() -> bool:
 			return true
 	return false
 	
+func scale_arrow() -> void:
+	var imp_len = get_impulse().length()
+	var percentage = imp_len / IMPULSE_MAX
+	arrow.scale.x = (_arrow_scale_x * percentage) + _arrow_scale_x
+	arrow.rotation = (_start - position).angle()
+	
+func play_stretch_sound() -> void:
+	if(_last_dragged_vector - _dragged_vector).length() > 0:
+		if stretch_sound.playing == false:
+			stretch_sound.play()
+	
 func get_dragged_vector(gmp:Vector2) -> Vector2:
 	return gmp - _drag_start
 
 func drag_in_limits() -> void:
+	
+	_last_dragged_vector = _dragged_vector
+	
 	_dragged_vector.x = clampf(_dragged_vector.x, DRAG_LIM_MIN.x, DRAG_LIM_MAX.x)
 	_dragged_vector.y = clampf(_dragged_vector.y, DRAG_LIM_MIN.y, DRAG_LIM_MAX.y)
 	position = _start + _dragged_vector
@@ -50,7 +86,9 @@ func update_drag() -> void:
 		return
 	var gmp = get_global_mouse_position()
 	_dragged_vector = get_dragged_vector(gmp)
+	play_stretch_sound()
 	drag_in_limits()
+	scale_arrow()
 
 func update(delta: float) -> void:
 	match _state:
